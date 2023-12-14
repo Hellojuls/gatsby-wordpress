593fdf42c406ff4443c3c6370b33630200268fff
hellojuls <julian.montani@gmail.com>
2023-12-14 21:12:03 +0100

    new file:   src/images/events.png 	new file:   src/images/events.svg 	deleted:    src/images/example.png 	deleted:    src/images/gatsby-icon.png

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,29 +15,29 @@ module.exports = {
     siteUrl: `https://montani.events/`,
   },
   plugins: [
-    // `gatsby-plugin-image`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     // This will impact how browsers show your PWA/website
-    //    // https://css-tricks.com/meta-theme-color-and-trickery/
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: ``, // This path is relative to the root of the site.
-    //   },
-    // },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/events.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
 }
